9d5c609fe274968ede6356b90f8c500ca6072cf7
Alisson C. Schmidt <>
2021-05-26 13:27:17 +0000

    entrega

---
+++ Funcoes.hs
@@ -0,0 +1,47 @@
+module Funcoes where
+
+sumSquares :: Int -> Int -> Int
+sumSquares x y = x^2 + y^2
+
+circleArea :: Float -> Float
+circleArea r = pi * r^2
+
+age :: Int -> Int -> Int
+age aN aT = aT - aN
+
+isElderly :: Int -> Bool
+isElderly x
+  | x >  65 = True
+  | x <= 65 = False
+
+htmlItem :: String -> String
+htmlItem str = "<li>" ++ str ++ "</li>"
+
+startsWithA :: String -> Bool
+startsWithA str = head(str) == 'A'
+
+isVerb :: String -> Bool
+isVerb str = last(str) == 'r'
+
+isVowel :: Char -> Bool
+isVowel char = elem char "aeiou"
+
+hasEqHeads :: [Int] -> [Int] -> Bool
+hasEqHeads lst1 lst2 = head(lst1) == head(lst2)
+
+isVowel2 :: Char -> Bool
+isVowel2 char = elem char "aeiouAEIOU"
+
+isLongWord :: String -> Bool
+isLongWord s = length s > 10
+
+isEvenBetter  :: Int -> Bool
+isEvenBetter n = mod n 2 == 0
+
+between60and80 :: Int -> Bool
+between60and80 int = int > 60 && int < 80
+
+achaEsp :: Char -> Int
+achaEsp c 
+   | c == ' '  = 1
+   | otherwise = 0
--- haskell01parte2.hs
+++ haskell01parte2.hs
@@ -1,2 +1,28 @@
 -- Prática 01 de Haskell - Parte 2
--- Nome: 
+-- Nome: Alisson Costa Schmidt
+
+import Funcoes
+
+itemize :: [String] -> [String]
+itemize str = map htmlItem str
+
+onlyVowels :: String -> String
+onlyVowels str = filter isVowel2 str
+
+onlyElderly :: [Int] -> [Int]
+onlyElderly int = filter isElderly int
+
+onlyLongWords :: [String] -> [String]
+onlyLongWords str = filter isLongWord str
+
+onlyEven :: [Int] -> [Int]
+onlyEven int = filter isEvenBetter int
+
+onlyBetween60and80 :: [Int] -> [Int]
+onlyBetween60and80 int = filter between60and80 int
+
+countSpaces :: String -> Int
+countSpaces x = sum (map achaEsp x)
+
+calcAreas :: [Float] -> [Float]
+calcAreas r = map circleArea r
